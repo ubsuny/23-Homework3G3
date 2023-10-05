08a9393425888c87c2b170dfb16dac717dc876fe
Sailesh Bataju <144289804+s4il3sh@users.noreply.github.com>
2023-10-05 13:10:16 -0400

    separated algorithm part
    As mentioned in the hw "seperate the algorithm part in its own library (module) file" it is done.

--- spring_const_graph.py
+++ spring_const_graph.py
@@ -1,19 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Experimental data
-masses = [20, 40, 60, 80, 100, 120]  # Masses in grams
-positions = [2.6, 5.4, 8.2, 10.8, 13.4, 16.1] # Extensions in cm
-elongations = [2.6, 5.4, 8.2, 10.8, 13.4, 16.1]  # Extensions in cm
-
 def calculate_spring_constants(masses, elongations):
     """
     Calculate spring constants from mass and extension data.
-#Calculation of spring constant
-spring_constant = lambda m,x: "ERROR" if x == 0 or m<0 else (m*g)/x
+    
+    # Calculation of spring constant
+    spring_constant = lambda m,x: "ERROR" if x == 0 or m<0 else (m*g)/x
     This function calculates spring constants using the provided mass and extension data and
     the formula (mass * acceleration due to gravity) / extension.
-# Given data (mass in grams and extension in centimeters)
+    # Given data (mass in grams and elongation in centimeters)
     Parameters:
         masses (list): A list of masses (in grams) placed on the spring.
         elongations (list): A list of extensions (in centimeters) of the spring from its equilibrium position.
@@ -21,10 +17,10 @@ spring_constant = lambda m,x: "ERROR" if x == 0 or m<0 else (m*g)/x
         list: A list of calculated spring constants.
     Note:
         The function assumes that the value of 'g' is approximately 981 cm/s^2.
-        If an extension is zero or the mass is negative, "ERROR" is returned for the corresponding spring constant.
+        If an elongation is zero or the mass is negative, "ERROR" is returned for the corresponding spring constant.
     Example:
         masses = [20, 40, 60, 80, 100, 120]  # Masses in grams
-        elongations = [2.6, 5.4, 8.2, 10.8, 13.4, 16.1]  # Extensions in cm
+        elongations = [2.6, 5.4, 8.2, 10.8, 13.4, 16.1]  # elongations in cm
         spring_constants = calculate_spring_constants(masses, elongations)
         # spring_constants will contain the calculated spring constants.
     """
@@ -37,8 +33,11 @@ spring_constant = lambda m,x: "ERROR" if x == 0 or m<0 else (m*g)/x
     # Calculate spring constants
     test_values = map(spring_constant, masses, elongations)
     spring_constants = list(test_values)
+    
+    avg = np.average(spring_constants)
+    output = print(f"The spring constant of a given spring is {avg:.2f} dyne/cm")
 
-    return spring_constants
+    return output 
 
 def plot_mass_vs_extension_with_trendline(masses, elongations):
     """
@@ -90,14 +89,3 @@ def plot_mass_vs_extension_with_trendline(masses, elongations):
 
     # Show the plot
     plt.show()
-
-#Printing outputs
-
-# Plot mass vs. extension with a best-fit trendline using the provided function
-plot_mass_vs_extension_with_trendline(masses, elongations)
-
-# Calculate spring constants using the provided function
-spring_constants = calculate_spring_constants(masses, elongations)
-
-avg = np.average(spring_constants)
-print(f"The spring constant of a given spring is {avg:.2f} dyne/cm")
