2957fb79f71fb0c4dbdd0e84e986f3d887477e53
Sailesh Bataju <144289804+s4il3sh@users.noreply.github.com>
2023-10-06 12:10:16 -0400

    Update main.py
    position constant was unnecessary might the result of merge requests

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import spring_const_graph as scg
 
 # Example data
 masses = [20, 40, 60, 80, 100, 120]  # Masses in grams
-positions = [2.6, 5.4, 8.2, 10.8, 13.4, 16.1] # Extensions in cm
 elongations = [2.6, 5.4, 8.2, 10.8, 13.4, 16.1]  # Extensions in cm
 
 scg.plot_mass_vs_extension_with_trendline(masses, elongations, 'best-fit.png')
